d42bfee31ad241e2bf5a622e0f84b5ab1030f89d
kingpin2k <kingpin2k@gmail.com>
2013-12-28 23:27:14 -0700

    Setting up nuspec file

--- HandlebarsHelper/Properties/AssemblyInfo.cs
+++ HandlebarsHelper/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("HandlebarsHelper")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Handlebars Helpers For .net developers")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("kingpin2k")]
 [assembly: AssemblyProduct("HandlebarsHelper")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
